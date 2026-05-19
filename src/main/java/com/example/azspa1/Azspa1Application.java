bfdd0398638ab4ef73ec366883d8ca7b43ce4c67
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-19 22:50:36 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/azspa1/Azspa1Application.java
@@ -0,0 +1,13 @@
+package com.example.azspa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Azspa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Azspa1Application.class, args);
+	}
+
+}
